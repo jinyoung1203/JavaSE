b789b491e7c3c4faac8e671357cde177f18eccef
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-14 19:49:09 +0900

    첫 커밋

---
+++ src/day01/hello/Hello.java
@@ -0,0 +1,14 @@
+package day01.hello;
+
+public class Hello {
+	//F11 단축키 console
+	public static void main(String[] args) {
+		System.out.println("Hello,World");
+		System.out.println();
+	}
+
+	}
+	
+	
+
+
---
+++ src/day02/test/MainClass01.java
@@ -0,0 +1,16 @@
+package day02.test;
+//주석이란 컴퓨터가 코드로 인식안함
+/*여러줄주석
+ * 
+ * 
+ */
+//한줄주석
+public class MainClass01{
+	//프로그램이 시작되는 메인메소드
+	//main ctrl space enter ↓
+	public static void main(String[] args) {
+		System.out.println("메인메소드 시작!");
+		//sysout ctrl space enter ↑
+	}
+
+}
